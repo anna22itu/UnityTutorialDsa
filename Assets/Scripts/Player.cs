fe994dbd2c3e527f1245909ffe7aa62be310984a
anna22itu <anna.iturralde.aguilo@gmail.com>
2022-11-22 13:47:01 +0100

    finalizado el tutorial

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,6 @@
-
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;        //Allows us to use SceneManager
-using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.UI;    //Allows us to use UI.
 
 //Player inherits from MovingObject, our base class for objects that can move, Enemy also inherits from this.
 public class Player : MovingObject
@@ -13,18 +9,19 @@ public class Player : MovingObject
     public int pointsPerFood = 10;                //Number of points to add to player food points when picking up a food object.
     public int pointsPerSoda = 20;                //Number of points to add to player food points when picking up a soda object.
     public int wallDamage = 1;                    //How much damage a player does to a wall when chopping it.
-    public Text foodText;
+    public Text foodText;                        //UI Text to display current player food total.
+    public AudioClip moveSound1;                //1 of 2 Audio clips to play when player moves.
+    public AudioClip moveSound2;                //2 of 2 Audio clips to play when player moves.
+    public AudioClip eatSound1;                    //1 of 2 Audio clips to play when player collects a food object.
+    public AudioClip eatSound2;                    //2 of 2 Audio clips to play when player collects a food object.
+    public AudioClip drinkSound1;                //1 of 2 Audio clips to play when player collects a soda object.
+    public AudioClip drinkSound2;                //2 of 2 Audio clips to play when player collects a soda object.
+    public AudioClip gameOverSound;                //Audio clip to play when player dies.
 
     private Animator animator;                    //Used to store a reference to the Player's animator component.
     private int food;                            //Used to store player food points total during level.
+    private Vector2 touchOrigin = -Vector2.one;    //Used to store location of screen touch origin for mobile controls.
 
-    public AudioClip moveSound1;
-    public AudioClip moveSound2;
-    public AudioClip eatSound1;
-    public AudioClip eatSound2;
-    public AudioClip drinkSound1;
-    public AudioClip drinkSound2;
-    public AudioClip mgameOverSound;
 
     //Start overrides the Start function of MovingObject
     protected override void Start()
@@ -35,6 +32,7 @@ public class Player : MovingObject
         //Get the current food point total stored in GameManager.instance between levels.
         food = GameManager.instance.playerFoodPoints;
 
+        //Set the foodText to reflect the current player food total.
         foodText.text = "Food: " + food;
 
         //Call the Start function of the MovingObject base class.
@@ -58,19 +56,62 @@ public class Player : MovingObject
         int horizontal = 0;      //Used to store the horizontal move direction.
         int vertical = 0;        //Used to store the vertical move direction.
 
+        //Check if we are running either in the Unity editor or in a standalone build.
+#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
 
         //Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
-        horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+        horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
 
         //Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
-        vertical = (int)(Input.GetAxisRaw("Vertical"));
+        vertical = (int) (Input.GetAxisRaw ("Vertical"));
 
         //Check if moving horizontally, if so set vertical to zero.
-        if (horizontal != 0)
+        if(horizontal != 0)
         {
             vertical = 0;
         }
+        //Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
 
+        //Check if Input has registered more than zero touches
+        if (Input.touchCount > 0)
+        {
+            //Store the first touch detected.
+            Touch myTouch = Input.touches[0];
+
+            //Check if the phase of that touch equals Began
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                //If so, set touchOrigin to the position of that touch
+                touchOrigin = myTouch.position;
+            }
+
+            //If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                //Set touchEnd to equal the position of this touch
+                Vector2 touchEnd = myTouch.position;
+
+                //Calculate the difference between the beginning and end of the touch on the x axis.
+                float x = touchEnd.x - touchOrigin.x;
+
+                //Calculate the difference between the beginning and end of the touch on the y axis.
+                float y = touchEnd.y - touchOrigin.y;
+
+                //Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+                touchOrigin.x = -1;
+
+                //Check if the difference along the x axis is greater than the difference along the y axis.
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                    //If x is greater than zero, set horizontal to 1, otherwise set it to -1
+                    horizontal = x > 0 ? 1 : -1;
+                else
+                    //If y is greater than zero, set horizontal to 1, otherwise set it to -1
+                    vertical = y > 0 ? 1 : -1;
+            }
+        }
+
+#endif //End of mobile platform dependendent compilation section started above with #elif
         //Check if we have a non-zero value for horizontal or vertical
         if (horizontal != 0 || vertical != 0)
         {
@@ -87,7 +128,9 @@ public class Player : MovingObject
         //Every time player moves, subtract from food points total.
         food--;
 
+        //Update food text display to reflect current score.
         foodText.text = "Food: " + food;
+
         //Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
         base.AttemptMove<T>(xDir, yDir);
 
@@ -143,7 +186,10 @@ public class Player : MovingObject
             //Add pointsPerFood to the players current food total.
             food += pointsPerFood;
 
+            //Update foodText to represent current total and notify player that they gained points
             foodText.text = "+" + pointsPerFood + " Food: " + food;
+
+            //Call the RandomizeSfx function of SoundManager and pass in two eating sounds to choose between to play the eating sound effect.
             SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
 
             //Disable the food object the player collided with.
@@ -156,7 +202,10 @@ public class Player : MovingObject
             //Add pointsPerSoda to players food points total
             food += pointsPerSoda;
 
+            //Update foodText to represent current total and notify player that they gained points
             foodText.text = "+" + pointsPerSoda + " Food: " + food;
+
+            //Call the RandomizeSfx function of SoundManager and pass in two drinking sounds to choose between to play the drinking sound effect.
             SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
 
             //Disable the soda object the player collided with.
@@ -169,8 +218,7 @@ public class Player : MovingObject
     private void Restart()
     {
         //Load the last scene loaded, in this case Main, the only scene in the game.
-        //Application.LoadLevel(Application.loadedLevel);
-        SceneManager.LoadScene(0);
+        Application.LoadLevel(Application.loadedLevel);
     }
 
 
@@ -184,6 +232,7 @@ public class Player : MovingObject
         //Subtract lost food points from the players total.
         food -= loss;
 
+        //Update the food display with the new total.
         foodText.text = "-" + loss + " Food: " + food;
 
         //Check to see if game has ended.
@@ -197,7 +246,10 @@ public class Player : MovingObject
         //Check if food point total is less than or equal to zero.
         if (food <= 0)
         {
-            SoundManager.instance.PlaySingle(mgameOverSound);
+            //Call the PlaySingle function of SoundManager and pass it the gameOverSound as the audio clip to play.
+            SoundManager.instance.PlaySingle(gameOverSound);
+
+            //Stop the background music.
             SoundManager.instance.musicSource.Stop();
 
             //Call the GameOver function of GameManager.
